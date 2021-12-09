7936c6c60049a6a2fe6e4121e11ea7b0a9d90cfb
afterrains <80660466+afterrainsss@users.noreply.github.com>
2021-12-09 13:49:44 +0700

    Add files via upload

---
+++ main.java
@@ -0,0 +1,15 @@
+package other;
+
+public class main {
+    public static void main(String[] args) {
+
+        System.out.println("RUMAH SAKIT HARAPAN KASIH");
+        RM rm = new RM();
+        rm.print();
+
+
+
+
+
+    }
+}
